5c56132b220e06755c203d8e257e44ac2e7db917
Julian Gamboa <Julian>
2020-05-28 18:21:18 -0500

    first commit

---
+++ bases/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modifica = models.DateTimeField(auto_now=True)
+    usuario_crea = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario_modifica = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        abstract=True
