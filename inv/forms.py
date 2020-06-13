31160db4800bfe779d4176cb6cb20eaf3e365ce5
Julian Gamboa <Julian>
2020-06-13 18:20:49 -0500

    Crear nueva categoria.

--- inv/forms.py
+++ inv/forms.py
@@ -15,7 +15,7 @@ class CategoriaForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in item(self.fields):
-            self.field[field].widget.attrs.update({
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
                 'class': 'form-control'
             })
